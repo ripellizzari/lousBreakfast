562d576ca558dd6e53ecc680e20437ed848a2de2
ripellizzari <ripellizzari@gmail.com>
2021-06-14 23:12:00 -0300

    First save

---
+++ app.js
@@ -0,0 +1,22 @@
+// addEventListener('DOMContentLoaded', () => {
+//     if (btn - menu) {
+//         btn - menu.addEventListener('click', () => {
+//             const menu_items = document.querySelector('.menu.items')
+//             menu_items.classList.toggle('show')
+
+//         })
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ carrito.js
@@ -0,0 +1,84 @@
+let carritoDeCompras = []
+
+const contenedorProducto = document.getElementById('contenedor-galeria')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const contadorCarrito = document.getElementById('contador-carrito')
+
+mostrarProductos(stockProductos)
+console.log(stockProductos)
+
+function mostrarProductos(array) {
+    array.forEach((x) => {
+        let div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML += ` 
+        <h3>${x.nombre}</h3>
+        <p>Precio: ${x.precio}</p>
+        <img src=${x.img} alt="" class="galeria__img">
+        <button id=${x.id} class="boton-agregar">Agregar<i class="fas fa-shopping-cart"></i></button>`
+
+        contenedorProducto.appendChild(div)
+
+        let boton = document.getElementById(`${x.id}`)
+        boton.addEventListener('click', () => { agregarAlCarrito(`${x.id}`) })
+    });
+
+
+
+
+}
+
+
+
+function agregarAlCarrito(id) {
+    let productoAgregar = stockProductos.filter(el => el.id == id)[0]
+    console.log(productoAgregar)
+    carritoDeCompras.push(productoAgregar)
+    actualizarCarrito()
+
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML += `
+    <p>${productoAgregar.nombre}</p>
+    <p>${productoAgregar.precio}</p>
+    <button id="${productoAgregar.id}"class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+    `
+    contenedorCarrito.appendChild(div)
+    let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+
+    botonEliminar.addEventListener('click', () => {
+        botonEliminar.parentElement.remove()
+        console.log(botonEliminar.parentElement)
+        carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id)
+        console.log(carritoDeCompras)
+        actualizarCarrito()
+    })
+}
+
+
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.length
+    //localStorage.setItem('carritoDeCompras', JSON.stringify(carritoDeCompras))
+    precioTotal.innerHTML = carritoDeCompras.reduce((acumulador, el) => acc + el.precio, 0)
+}
+
+
+function revisarLocal() {
+    let carritoLocal = JSON.parse(localStorage.getItem('carritoDeCompras'))
+    if (carritoLocal) {
+        carritoLocal.forEach((el) => agregarAlCarrito(el.id))
+    }
+}
+
+revisarLocal()
+
+
+
+
+
+
+
+
+
+
