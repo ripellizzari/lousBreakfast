39f7f504f2e5a0bd285a60ba491fe5c85fbe979f
ripellizzari <ripellizzari@gmail.com>
2021-06-15 19:17:59 -0300

    Primeros JQuery

--- app.js
+++ app.js
@@ -8,13 +8,19 @@
 //     }
 // })
 
+// $('#definicion').append("<p>Un lugar pensado para que puedas personalizar al máximo tu experiencia. Podrás elegir entre nuestra variedad de productos y armar tu desayuno como más te guste. También tendrás la posibilidad de aprovechar nuestra modalidad de Take-Away, y disfrutar de tu café al aire libre. Además, con LOU'S podrás sorprender a esa persona tanimportante para vos, con nuestros envios personalizados, hechos a medida, de acuerdo sus gustos.</p>")
 
 
+var resultado = $('#resultado')
+
+$('#titulo').click(function () {
+    resultado.toggle(1200);
+})
+
+$('#titulo').css("font-size: 50px, margin:20px, background-color: EQBBE4 ")
 
 
-
-
-
+$('#redes').prepend("<h2>SEGUINOS</h2>");
 
 
 
