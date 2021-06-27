5d28ec18c83e001333800cfe1df03a7d003dce3d
ripellizzari <ripellizzari@gmail.com>
2021-06-27 20:47:07 -0300

    Modal Carrito

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-// addEventListener('DOMContentLoaded', () => {
-//     const btn_menu = document.querySelector('.btn_menu')
-//     if (btn_menu) {
-//         btn_menu.addEventListener('click', () => {
-//             const menu_items = document.querySelector('.menu-items')
-//             menu_items.classList.toggle('show')
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu-items')
+            menu_items.classList.toggle('show')
 
-//         })
-//     }
-// })
+        })
+    }
+})
+
+/********************************************************************** */
+
 
 $('#definicion').append("<p>Un lugar pensado para que puedas personalizar al máximo tu experiencia. Podrás elegir entre nuestra variedad de productos y armar tu desayuno como más te guste. También tendrás la posibilidad de aprovechar nuestra modalidad de Take-Away, y disfrutar de tu café al aire libre. Además, con LOU'S podrás sorprender a esa persona tanimportante para vos, con nuestros envios personalizados, hechos a medida, de acuerdo sus gustos.</p>")
 
-
-/********************************************************************** */
 
 var resultado = $('#resultado')
 
@@ -24,8 +25,4 @@ $('#titulo').css("font-size: 50px, margin:20px, background-color: EQBBE4 ")
 
 
 $('#contacto').prepend("<h2>SEGUINOS</h2>");
-
-
-
-
 
--- carrito.js
+++ carrito.js
@@ -10,8 +10,6 @@ const precioTotal = document.getElementById('precioTotal')
 
 // Estructura de display de productos
 let stockProductos = []
-// mostrarProductos(stockProductos)
-// console.log(stockProductos)
 
 $.getJSON('stock.json', function mostrarProductos(data) {
     data.forEach((x) => {
@@ -38,11 +36,7 @@ $.getJSON('stock.json', function mostrarProductos(data) {
 
 
 function agregarAlCarrito(id) {
-    // let validar = carritoDeCompras.some(x => x.id == id)
-    // if (validar) {
-    //     contadorCarrito.innerText = carritoDeCompras.length++
-    // }
-    // else {
+   
     let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
     console.log(productoAgregar)
     carritoDeCompras.push(productoAgregar)
@@ -68,40 +62,6 @@ function agregarAlCarrito(id) {
         actualizarCarrito()
     })
 }
-
-
-
-
-// function agregarAlCarrito(id) {
-//     let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
-//     console.log(productoAgregar)
-//     carritoDeCompras.push(productoAgregar)
-//     // actualizarCarrito()
-
-//     let div = document.createElement('div')
-//     div.classList.add('productoEnCarrito')
-//     div.innerHTML += `
-//     <p>${productoAgregar.nombre}</p>
-//     <p>Precio: ${productoAgregar.precio}</p>
-//     <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-//     `
-
-//     contenedorCarrito.appendChild(div)
-//     let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
-
-
-//     botonEliminar.addEventListener('click', () => {
-//         botonEliminar.parentElement.remove()
-//         console.log(botonEliminar.parentElement)
-//         carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id)
-//         console.log(carritoDeCompras)
-//         actualizarCarrito()
-//     })
-// }
-
-
-
-
 
 
 
